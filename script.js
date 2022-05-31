d3c86d479f363ec5d968bb17c817af9941df892b
Viral-Gajera <viral.gajera218@gmail.com>
2022-05-31 20:27:18 +0530

    version 1.0.0
    'clock' is working
    'World Clock', 'StopWatch', 'Timer' is not working

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ let minutes = document.getElementById('minutes');
 let second = document.getElementById('second');
 let ampm = document.getElementById('AMPM');
 
+
+
 setInterval( function(){
     
     let now = new Date();
-    
     let h = now.getHours();
 
+    // logic for digital clock
     if(h>12){
         ampm.innerHTML = 'PM';
         h = h - 12;
@@ -32,4 +34,41 @@ setInterval( function(){
         minimumIntegerDigits: 2,
         useGrouping: false
       });
-}, 1000 )
+
+
+    // logic for analogue clock
+}, 1000 );
+
+
+let hour_animation = document.getElementById('hour-animation');
+let minute_animation = document.getElementById('minute-animation');
+let second_animation = document.getElementById('second-animation');
+
+// second_animation.style.transform = `rotate(${270}deg)`;
+// minute_animation.style.transform = `rotate(${270}deg)`;
+// hour_animation.style.transform = `rotate(${270}deg)`;
+
+let now = new Date();
+// now.setHours(12);
+// now.setMinutes(00);
+// now.setSeconds(0);
+
+let h = ( now.getHours() > 12 ? (now.getHours() - 12) : now.getHours() );
+let m = now.getMinutes();
+let s = now.getSeconds();
+
+console.log(h);
+
+    h = 30*h + (m/2) + (s/120);
+    m = 6*m + (s/10);
+    s = 6*s + 6;
+
+
+setInterval(function(){
+  hour_animation.style.transform = `rotate(${h}deg)`;
+  minute_animation.style.transform = `rotate(${m}deg)`;
+  second_animation.style.transform = `rotate(${s}deg)`;
+  s += 6;
+  m += 0.1;
+  h += 0.0083333;
+}, 1000)
