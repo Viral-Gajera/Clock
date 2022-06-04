e902eff0b0a78d332116fad7fb534840bdc90628
Viral-Gajera <viral.gajera218@gmail.com>
2022-06-04 22:55:20 +0530

    link changed in common javascript file

--- common1.js
+++ common1.js
@@ -3,16 +3,16 @@ let nav_bar = document.querySelector('.nav-bar');
 let nav_bar_content = `
 <ul>
     <li class="active">
-        <a href="./clock.html">Clock</a>
+        <a href="../clock/clock.html">Clock</a>
     </li>
     <li> 
-        <a href="./world-clock.html">World Clock</a> 
+        <a href="../world-clock/world-clock.html">World Clock</a> 
     </li>
     <li> 
-        <a href="./stopwatch.html">StopWatch</a> 
+        <a href="../stopwatch/stopwatch.html">StopWatch</a> 
     </li>
     <li> 
-        <a href="./timer.html">Timer</a> 
+        <a href="../timer/timer.html">Timer</a> 
     </li>
 </ul>`;
 
